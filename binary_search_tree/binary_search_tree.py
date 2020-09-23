42f382a27423d09207eb64a69336ee900e1a8378
iesous-kurios <j.wylie.81@gmail.com>
2020-09-22 20:44:48 -0700

    finished day 1 of BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,8 @@
+import sys
+path = sys.path.append('..')
+from queue import Queue
+from stack.stack import Stack   
+
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -17,20 +22,50 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        parent = self
+        node = BSTNode(value)
+        while True:
+            if value < parent.value:
+                if parent.left:
+                    parent = parent.left  # traverse down left
+                else:
+                    parent.left = node
+                    break
+            else:
+                if parent.right:
+                    parent = parent.right  # traverse down right
+                else:
+                    parent.right = node
+                    break
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        node = self
+        while node.value != target:
+            if target < node.value and node.left:
+                node = node.left
+            elif target >= node.value and node.right:
+                node = node.right
+            else:
+                break
+        return node.value == target
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        node = self
+        while node.right:
+            node = node.right
+        return node.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        node = self
+        cb(node.value)
+        if node.left:
+            node.left.for_each(fn)
+        if node.right:
+            node.right.for_each(fn)
 
     # Part 2 -----------------------
 
@@ -80,6 +115,6 @@ print("elegant methods")
 print("pre order")
 bst.pre_order_dft()
 print("in order")
-bst.in_order_dft()
+bst.in_order_print()
 print("post order")
 bst.post_order_dft()  
